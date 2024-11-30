7e59c48ae8de2fe3d9f87df76e4f39643362e84e
Aayush Gautam <aayushgtm5@gmail.com>
2024-11-30 17:25:13 +0545

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { HomePageComponent } from "@/components/home-page";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-background">
+      <HomePageComponent />
+    </div>
+  );
+}
